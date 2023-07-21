d330e6e4e8c21cf8557681b48a6c3df3518e6dc6
Mobin Panahandeh <pnamobin@gmail.com>
2023-07-22 01:09:53 +0330

    Solve the problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(start > end){
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    if(start < 0 || end <= 0 || start == NaN || end == NaN){
+        return 'ERROR';
+    }
+    let sum=0;
+    for (start; start <= end; start++){
+        sum += start;
+    }
+    return sum;
 };
 
 // Do not edit below this line
