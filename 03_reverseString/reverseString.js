4bbe87547b48b8cee659f110022b883426e8b510
Mobin Panahandeh <pnamobin@gmail.com>
2023-07-21 22:28:16 +0330

    Solve the problem

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strLength = str.length - 1;
+    let word = '';
+    for(strLength; strLength > -1; strLength--){
+        word += str[strLength];
+    }
+    return word;
 };
 
 // Do not edit below this line
