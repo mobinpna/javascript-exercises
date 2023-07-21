826fd5d8a81c24c82fd54fe9e508b012103e2127
Mobin Panahandeh <pnamobin@gmail.com>
2023-07-21 21:52:24 +0330

    Solve the problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string , num) {
+    
+    let sameString = string;
 
+    if(num == 0){
+        return '';
+    }
+
+    if(num < 0){
+        return 'ERROR'
+    }
+
+    for (let i = 0; i < num-1 ; i++){
+        sameString = sameString+string;
+    }
+    return sameString;
 };
 
 // Do not edit below this line
