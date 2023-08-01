74b1a7bf10478bc7380e54328217e63640026875
Mobin Panahandeh <pnamobin@gmail.com>
2023-08-01 14:30:21 +0330

    Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a<0)return "OOPS"
+    array = [1,1];
+    for (let i= 2; i < a; i++){
+        array[i] = array [i-1] + array [i-2];
+    }
+    return array[a-1];
 };
 
 // Do not edit below this line
