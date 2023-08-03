d8066649a302bad076ea988c9e55267a36eb7fe4
Mobin Panahandeh <pnamobin@gmail.com>
2023-08-03 12:08:31 +0330

    Solve the problem

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(a) {
+    let outIndex =0;
+    a.reduce((age, person, index) => {
+        if(!person.yearOfDeath) person.yearOfDeath = new Date().getFullYear()
+        if(person.yearOfDeath-person.yearOfBirth > age) {
+            outIndex = index;
+        }
+        else {
+            age = person.yearOfDeath-person.yearOfBirth;
+        }
+        return person.yearOfDeath-person.yearOfBirth;
+    },0);
+    return a[outIndex]
 };
 
 // Do not edit below this line
